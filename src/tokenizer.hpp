3794313752a1f886d613d715c7b5a6902946c51b
ivanrj7j <ivanrj7j@gmail.com>
2025-11-28 15:56:05 +0530

    started parsing strings, characters and comments

--- src/tokenizer.hpp
+++ src/tokenizer.hpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include "tokens.hpp"
 
 using namespace std;
 
@@ -12,7 +13,7 @@ class Tokenizer{
 
 private:
     string fileName;
-    vector<string> tokens;
+    vector<Token> tokens;
 
     string readCode(){
         stringstream content;
@@ -29,18 +30,76 @@ private:
     void getTokens(){
         string sourceCode = readCode();
         string currentToken;
+        char current;
 
         tokens.clear();
-        for(int i = 0; i < sourceCode.length(); i++){
-            if(sourceCode.at(i) == ' ' || sourceCode.at(i) == '\t' || sourceCode.at(i) == '\n'){
-                this->tokens.push_back(currentToken);
-                currentToken.clear();
-                continue;
-            }
 
-            currentToken.push_back(sourceCode.at(i));
+        bool commentMode = false; // true if currently parsing a comment
+        bool stringMode = false; // true if currently parsing a string
+        bool charMode = false; // true if currently parsing a character
+        
+        for(int i = 0; i < sourceCode.length(); i++){
+            char current = sourceCode.at(i);
+
+            if(current == '#' && !(stringMode || charMode || commentMode)){
+                currentToken.clear(); // TODO: have better error handling, this will cause problems in future, here just for initial testing purposes
+                commentMode = true;
+                continue;
+            } // if # is found and we are not parsing a string, then the current token is a comment, so comment mode is turned on.
+
+            if(current == '"' && !(commentMode || charMode)){
+                if(stringMode){
+                    stringMode = false;
+                    tokens.push_back(Token(LiteralType{_stringLit}, currentToken));
+                    currentToken.clear();
+                    // if stringmode is on and " is found, it means the string is ending
+                }else{
+                    currentToken.clear(); // TODO: have better error handling, this will cause problems in future, here just for initial testing purposes
+                    stringMode = true;
+                    // starting to parse a string 
+                    continue;
+                }
+            } // if " is found and we are not parsing a comment, then the current token is a string, so string mode is turned on. there are two possibilities, either the string is starting or ending, both cases are handled inside the code.
+
+            if(current == '\'' && !(commentMode || stringMode)){
+                if(charMode){
+                    charMode = false;
+                    if(currentToken.length() != 1){
+                        cerr << "The length of a character should exactly be 1.";
+                        throw invalid_argument("The length of the character should exactly be 1.");
+                    }// if the given character token does not have one character, then it is invalid.
+                    tokens.push_back(Token(LiteralType{_charLit}, currentToken));
+                    currentToken.clear();
+                }else{
+                    currentToken.clear(); // TODO: have better error handling, this will cause problems in future, here just for initial testing purposes
+                    charMode = true;
+                    continue;
+                }
+            }//same logic used for string parsing
+
+            if(current == '\n'){
+                cout << "Newline character found: commentMode is " << (commentMode ? "on": "off") << endl;
+                if(commentMode){
+                    commentMode = false;
+                    tokens.push_back(Token(currentToken));
+                    currentToken.clear();
+                }
+                tokens.push_back(Token(WhiteSpaceType{_newLine}));
+            }// terminating a comment
+
+            if(!(commentMode || charMode || stringMode) && (current == '\t' || current == ' ')){
+                WhiteSpaceType type = _space;
+                if(current == '\t') type = _tab;
+                tokens.push_back(Token(type));
+                continue;
+            }// pushing in whitespaces
+
+            currentToken.push_back(current);
+            
+            cout << "Parsed '" << current << "' Current Tokens: " << endl;
+            printTokens();
+            cout << endl;
         }
-        if(!currentToken.empty()) this->tokens.push_back(currentToken);
     }
 
 public:
@@ -49,10 +108,27 @@ public:
         cout << "Initialized Tokenizer" << endl;
         this->getTokens();
         cout << "Tokens generated:" << endl;
-        for(string token: this->tokens){
-            cout << token << endl;
+        printTokens();
+    }
+    
+    void printTokens(){
+        for(Token token: tokens){
+            _Token t = token.get();
+            switch (t.tokenType)
+            {
+            case _comment:
+                cout << "\nComment: " << t.value << endl;
+                break;
+            case _literal:
+                cout << "\nLiteral: " << t.value << endl;
+                break;
+            case _whitespace:
+                cout << "Whitespace Type enum: " << t.token << endl;
+                break;
+            }
         }
     }
+
     ~Tokenizer(){
         cout << "Exiting...";
     }
---
+++ src/tokens.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+enum TokenType {_keyWord, _identifier, _literal, _operator, _delimiter, _comment, _whitespace};
+enum KeyWordType {_if, _elif, _else, _for, _while, _return, _int, _float, _string};
+enum LiteralType {_intLit, _floatLit, _stringLit, _charLit};
+enum OperatorType {_add, _sub, _mul, _div, _mod, _and, _or, _not, _eq, _neq};
+enum DelimiterType {_bracketOpen, _bracketClose, _braceOpen, _braceClose, _sqOpen, _sqClose};
+enum WhiteSpaceType {_space, _tab, _newLine};
+
+class _Token{
+    public:
+        TokenType tokenType;
+        int16_t token;
+        string value;
+
+        _Token(TokenType tokenType, int16_t token, string value){
+            this->tokenType = tokenType;
+            this->token = token;
+            this->value = value;
+        }
+};
+
+class Token{
+    private:
+        TokenType tokenType;
+        KeyWordType keywordType;
+        LiteralType literalType;
+        OperatorType operatorType;
+        DelimiterType delimiterType;
+        WhiteSpaceType whiteSpaceType;
+        string token;
+
+    public:
+        Token(KeyWordType keywordType, string token){
+            this->tokenType = _keyWord;
+            this->keywordType = keywordType;
+            this->token = token;
+        }
+
+        Token(LiteralType literalType, string token){
+            this->literalType = literalType;
+            this->tokenType = _literal;
+            this->token = token;
+        }
+
+        Token(OperatorType operatorType){
+            this->tokenType = _operator;
+            this->operatorType = operatorType;
+        }
+
+        Token(DelimiterType delimiterType){
+            this->tokenType = _delimiter;
+            this->delimiterType = delimiterType;
+        }
+
+        Token(WhiteSpaceType whiteSpaceType){
+            this->tokenType = _whitespace;
+            this->whiteSpaceType = whiteSpaceType;
+        }
+
+        Token(string comment){
+            this->token = comment;
+            this->tokenType = _comment;
+        }
+
+        _Token get(){
+            int16_t token;
+            switch (this->tokenType)            {
+            case _keyWord:
+                token = this->keywordType;
+                break;
+            case _literal:
+                token = this->literalType;
+                break;
+            case _operator:
+                token = this->operatorType;
+                break;
+            case _delimiter:
+                token = this->delimiterType;
+                break;
+            case _whitespace:
+                token = this->whiteSpaceType;
+                break;
+            case _comment:
+                break;
+            default:
+                throw invalid_argument("The type of token given is incorrect");
+            }
+            return _Token(this->tokenType, token, this->token);
+        }
+};
