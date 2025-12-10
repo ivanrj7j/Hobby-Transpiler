b5d42dacd58559d2897a9cef8f08163de62f8727
ivanrj7j <ivanrj7j@gmail.com>
2025-12-10 20:11:56 +0530

    migrated to default cpp formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
-#include <iostream>
 #include "tokenizer.hpp"
+#include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]){
-    if(argc != 2){
-        cerr << "HoPiler failed. No source coude given! When running the code, also include the filename like:" << endl << "HoPiler fileName.ho";
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        cerr << "HoPiler failed. No source coude given! When running the code, also include the filename like:" << endl
+             << "HoPiler fileName.ho";
         return EXIT_FAILURE;
     }
 
     string fileName = string(argv[1]);
     Tokenizer tokenizer(fileName);
-    
+
     return EXIT_SUCCESS;
 }
--- src/tokens.hpp
+++ src/tokens.hpp
@@ -3,102 +3,162 @@
 #include <string>
 using namespace std;
 
-enum TokenType {_keyWord, _identifier, _literal, _operator, _delimiter, _comment, _whitespace};
-enum KeyWordType {_if, _elif, _else, _for, _while, _do, _return, _break, _continue, _int, _float, _string, _char, _bool};
-enum LiteralType {_intLit, _floatLit, _stringLit, _charLit};
-enum OperatorType {_add, _sub, _mul, _div, _mod, _pow, _and, _or, _not, _xor, _eq, _neq, _gte, _lte, _gt, _lt, _ass, _assAdd, _assSub, _assMul, _assDiv, _assMod, _assPow};
-enum DelimiterType {_bracketOpen, _bracketClose, _braceOpen, _braceClose, _sqOpen, _sqClose};
-enum WhiteSpaceType {_space, _tab, _newLine};
+enum TokenType { _keyWord,
+    _identifier,
+    _literal,
+    _operator,
+    _delimiter,
+    _comment,
+    _whitespace };
+enum KeyWordType { _if,
+    _elif,
+    _else,
+    _for,
+    _while,
+    _do,
+    _return,
+    _break,
+    _continue,
+    _int,
+    _float,
+    _string,
+    _char,
+    _bool };
+enum LiteralType { _intLit,
+    _floatLit,
+    _stringLit,
+    _charLit };
+enum OperatorType { _add,
+    _sub,
+    _mul,
+    _div,
+    _mod,
+    _pow,
+    _and,
+    _or,
+    _not,
+    _xor,
+    _eq,
+    _neq,
+    _gte,
+    _lte,
+    _gt,
+    _lt,
+    _ass,
+    _assAdd,
+    _assSub,
+    _assMul,
+    _assDiv,
+    _assMod,
+    _assPow };
+enum DelimiterType { _bracketOpen,
+    _bracketClose,
+    _braceOpen,
+    _braceClose,
+    _sqOpen,
+    _sqClose };
+enum WhiteSpaceType { _space,
+    _tab,
+    _newLine };
 
-class _Token{
-    public:
-        TokenType tokenType;
-        int16_t token;
-        string value;
+class _Token {
+public:
+    TokenType tokenType;
+    int16_t token;
+    string value;
 
-        _Token(TokenType tokenType, int16_t token, string value){
-            this->tokenType = tokenType;
-            this->token = token;
-            this->value = value;
-        }
+    _Token(TokenType tokenType, int16_t token, string value)
+    {
+        this->tokenType = tokenType;
+        this->token = token;
+        this->value = value;
+    }
 };
 
-class Token{
-    private:
-        TokenType tokenType;
-        KeyWordType keywordType;
-        LiteralType literalType;
-        OperatorType operatorType;
-        DelimiterType delimiterType;
-        WhiteSpaceType whiteSpaceType;
-        string token;
+class Token {
+private:
+    TokenType tokenType;
+    KeyWordType keywordType;
+    LiteralType literalType;
+    OperatorType operatorType;
+    DelimiterType delimiterType;
+    WhiteSpaceType whiteSpaceType;
+    string token;
 
-    public:
-        Token(){};
+public:
+    Token() { };
 
-        Token(KeyWordType keywordType){
-            this->tokenType = _keyWord;
-            this->keywordType = keywordType;
-        }
+    Token(KeyWordType keywordType)
+    {
+        this->tokenType = _keyWord;
+        this->keywordType = keywordType;
+    }
 
-        Token(LiteralType literalType, string value){
-            this->literalType = literalType;
-            this->tokenType = _literal;
-            this->token = value;
-        }
+    Token(LiteralType literalType, string value)
+    {
+        this->literalType = literalType;
+        this->tokenType = _literal;
+        this->token = value;
+    }
 
-        Token(OperatorType operatorType){
-            this->tokenType = _operator;
-            this->operatorType = operatorType;
-        }
+    Token(OperatorType operatorType)
+    {
+        this->tokenType = _operator;
+        this->operatorType = operatorType;
+    }
 
-        Token(DelimiterType delimiterType){
-            this->tokenType = _delimiter;
-            this->delimiterType = delimiterType;
-        }
+    Token(DelimiterType delimiterType)
+    {
+        this->tokenType = _delimiter;
+        this->delimiterType = delimiterType;
+    }
 
-        Token(WhiteSpaceType whiteSpaceType){
-            this->tokenType = _whitespace;
-            this->whiteSpaceType = whiteSpaceType;
-        }
+    Token(WhiteSpaceType whiteSpaceType)
+    {
+        this->tokenType = _whitespace;
+        this->whiteSpaceType = whiteSpaceType;
+    }
 
-        Token(string comment){
-            this->token = comment;
-            this->tokenType = _comment;
-        }
-        
-        _Token get(){
-            int16_t token;
-            switch (this->tokenType){
-            case _keyWord:
-                token = this->keywordType;
-                break;
-            case _literal:
-                token = this->literalType;
-                break;
-            case _operator:
-                token = this->operatorType;
-                break;
-            case _delimiter:
-                token = this->delimiterType;
-                break;
-            case _whitespace:
-                token = this->whiteSpaceType;
-                break;
-            case _comment:
-                break;
-            case _identifier:
-                break;
-            default:
-                throw invalid_argument("The type of token given is incorrect");
-            }
-            return _Token(this->tokenType, token, this->token);
-        }
+    Token(string comment)
+    {
+        this->token = comment;
+        this->tokenType = _comment;
+    }
 
-        static Token identifier(string name){
-            Token token;
-            token.tokenType = _identifier;
-            token.token = name;
-            return token;
+    _Token get()
+    {
+        int16_t token;
+        switch (this->tokenType) {
+        case _keyWord:
+            token = this->keywordType;
+            break;
+        case _literal:
+            token = this->literalType;
+            break;
+        case _operator:
+            token = this->operatorType;
+            break;
+        case _delimiter:
+            token = this->delimiterType;
+            break;
+        case _whitespace:
+            token = this->whiteSpaceType;
+            break;
+        case _comment:
+            break;
+        case _identifier:
+            break;
+        default:
+            throw invalid_argument("The type of token given is incorrect");
         }
+        return _Token(this->tokenType, token, this->token);
+    }
+
+    static Token identifier(string name)
+    {
+        Token token;
+        token.tokenType = _identifier;
+        token.token = name;
+        return token;
+    }
 };
